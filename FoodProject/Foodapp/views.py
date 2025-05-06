946dfc0503ca96de8206d74421438d21321485f6
SnehaPatil1512 <snmtppatil@gmail.com>
2025-05-06 14:18:07 +0530

    Django Practice

---
+++ FoodProject/Foodapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from Foodapp.models import Item
+# Create your views here.
+def home(request):
+    li = Item.objects.all()
+    context = {
+        'li': li,
+    }
+    return render(request, 'home.html', context)
+
+def details(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item': item
+    }
+    return render(request, 'details.html', context)
